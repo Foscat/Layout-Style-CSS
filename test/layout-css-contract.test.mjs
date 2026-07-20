a99fdf7043002e1a5e0358f8b42105b60419ac38
Foscat <fosterkyle6356@gmail.com>
2026-07-20 18:39:31 -0500

    test: refresh UI style kit fixture

--- test/layout-css-contract.test.mjs
+++ test/layout-css-contract.test.mjs
@@ -603,7 +603,7 @@ const packageJson = JSON.parse(readFileSync(join(root, "package.json"), "utf8"))
 const packageLock = JSON.parse(readFileSync(join(root, "package-lock.json"), "utf8"));
 const lockRoot = packageLock.packages[""];
 const uiKitFixtureSpec = "git+https://github.com/Foscat/ui-style-kit-css.git#2.1.0";
-const uiKitFixtureCommit = "76eff938513d9a39389c0db8df2271aafa3294e1";
+const uiKitFixtureCommit = "3516e7254b540b4141bbacdb2e5775a2a0f1dcfb";
 const uiKitFixturePackage = JSON.parse(
   readFileSync(join(root, "node_modules", "ui-style-kit-css", "package.json"), "utf8")
 );
